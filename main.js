2ce3596f6964e7f97abd789151b3b1b92f64ab07
Nailyam Tokhtamova <nailyamtokhtamova@Nailyams-MacBook-Air.local>
2023-04-30 20:00:47 +0600

    commit

--- main.js
+++ main.js
@@ -26,3 +26,11 @@ navbarToggle.addEventListener('click', () => {
   navbarMenu.classList.toggle('active');
 });
 
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+
+hamburger.addEventListener("click", () => {
+  
+  navLinks.classList.toggle("active");
+});
